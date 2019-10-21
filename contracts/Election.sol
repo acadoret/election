f5266eb16bffa49c0360fbbdefc25d52396b4814
acadoret <amaruka@protonmail.com>
2019-10-21 21:19:13 +0200

    Ajout du systeme de vote + test + rendu web

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.8;
+ pragma solidity 0.5.8;
 
 contract Election {
     // Model a candidate
@@ -13,19 +13,31 @@ contract Election {
     mapping(uint => Candidate) public candidates;
     // Store Candidates Count
     uint public candidatesCount;
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store voters count
+    uint public votersCount;
 
     // Constructor
     constructor() public {
-        addCandidate("Candidate 1"); 
+        addCandidate("Candidate 1");
         addCandidate("Candidate 2");
     }
 
     function addCandidate(string memory _name) private {
         candidatesCount ++;
-        candidates[candidatesCount] = Candidate(
-            candidatesCount, 
-            _name, 
-            0
-        );
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender],"Voter can't vote twice");
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount, "Your vote doesn't match witch any candidate");
+        // record that voter has voted
+        voters[msg.sender] = true;
+        votersCount ++;
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
     }
 }
